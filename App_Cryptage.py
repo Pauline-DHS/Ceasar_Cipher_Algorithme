3dd8dc711274ce1ebfa44ec5e1d443aa144d339d
Pauline-DHS <pauline.dehors789@gmail.com>
2022-10-13 22:22:25 +0200

    Mise à jour des algorithmes de cryptage et de décryptage avec le principe de Ceasar Cipher

--- App_Cryptage.py
+++ App_Cryptage.py
@@ -7,43 +7,73 @@ class App(tk.Tk):
 		super().__init__()
 		self.title('Application de Crypatge')
 		self.geometry("800x600")
-
 		self.name_var = tk.StringVar() 
+		self.entier_var = tk.IntVar()
 		
-		self.columnconfigure(0, weight=1)
-		self.columnconfigure(1, weight=1)
-		self.columnconfigure(2, weight=1)
+		self.config(bg='#0B3948')
 	
 		self.create_widgets()
 		
 	def create_widgets(self):
 
-		padding = {'padx': 10, 'pady': 10}
+		#padding = {'padx': 10, 'pady': 10}
 		# label
-		ttk.Label(self, text='Votre chaîne de caractères:').place(x=100, y=100)
-
+		label1 = ttk.Label(self, text='Votre chaîne de caractères :', background='#0B3948', font='bold').place(x=300, y=220)
+		
+		
 		# Entry
 		name_entry = ttk.Entry(self, textvariable=self.name_var)
-		name_entry.place(x=300, y=100)
+		name_entry.place(x=190, y=250)
 		name_entry.config(width=50)
-		name_entry.focus()
 
 		# Button
-		submit_button_encrypter = ttk.Button(self, text='Encrypter', command=self.submit)
-		submit_button_encrypter.place(x=250, y=200)
+		submit_button_encrypter = ttk.Button(self, text='Encrypter',command=self.encryptage)
+		submit_button_encrypter.place(x=220, y=320)
+     
 
-		submit_button_decrypter = ttk.Button(self, text='Décrypter', command=self.submit)
-		submit_button_decrypter.place(x=450, y=200)
+		submit_button_decrypter = ttk.Button(self, text='Décrypter', command=self.decryptage)
+		submit_button_decrypter.place(x=470, y=320)	
 
-		# Output label
-		self.output_label = ttk.Label(self)
-		self.output_label.place(x=300, y=100)
-
-
-	def submit(self):
-		self.output_label.config(text=self.name_var.get())
+		
 
 
+	def encryptage(self):
+		Messageacrypter=self.name_var.get()
+		cle=24 # Décalage par rapport à Y (code ASCII : 24 + 1 = 25e lettre de l'alphabet)
+
+		acrypter=Messageacrypter.upper()
+		lg=len(acrypter)
+		MessageCrypte=""
+
+		for i in range(lg):
+			if acrypter[i]==' ':
+				MessageCrypte+=' '
+			else:
+				asc=ord(acrypter[i])+cle
+				MessageCrypte+=chr(asc+26*((asc<65)-(asc>90)))
+
+		print(MessageCrypte)
+		self.output_label=ttk.Label(self, text=MessageCrypte, background='#ACB0BD', font='bold').place(x=370, y=400)
+		self.label2 = ttk.Label(self, text='Votre chaîne encryptée/décryptée :', background='#0B3948', font='bold').place(x=50, y=400)
+
+	def decryptage(self):
+		MessageCrypte=self.name_var.get()
+		lg=len(MessageCrypte)
+		MessageClair=""
+		cle=24 # Décalage par rapport à Y (code ASCII : 24 + 1 = 25e lettre de l'alphabet)
+
+		for i in range(lg):
+			if MessageCrypte[i]==' ':
+				MessageClair+=' '
+			else:
+				asc=ord(MessageCrypte[i])-cle
+				MessageClair+=chr(asc+26*((asc<65)-(asc>90)))
+
+		print(MessageClair)
+		self.label2 = ttk.Label(self, text='Votre chaîne encryptée/décryptée :', background='#0B3948', font='bold').place(x=50, y=400)
+		self.output_label=ttk.Label(self, text=MessageClair, background='#ACB0BD', font='bold').place(x=370, y=400)
+		
+		
 if __name__ == "__main__":
 	app = App()
 	app.mainloop()
